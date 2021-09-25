092fd06b7ea9836fb40ddcf8af863e687f2049dc
ikalco <73481042+ikalco@users.noreply.github.com>
2021-09-24 21:19:57 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -55,3 +55,4 @@ function draw() {
 }
 
 // <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.min.js" integrity="sha512-N4kV7GkNv7QR7RX9YF/olywyIgIwNvfEe2nZtfyj73HdjCUkAfOBDbcuJ/cTaN04JKRnw1YG1wnUyNKMsNgg3g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+// https://gamedev.stackexchange.com/questions/148685/how-do-you-go-about-distributing-resources-procedurally-in-a-game
